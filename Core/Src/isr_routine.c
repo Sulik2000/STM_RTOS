24ef3266c411317426c596f8aab98ad54526f392
Stanislav Suhlobov <s.suglobov@sirinsoftware.com>
2026-07-06 14:55:40 +0300

    Add raw bootloader to boot main() function

---
+++ Core/Src/isr_routine.c
@@ -0,0 +1,33 @@
+#include "isr_routine.h"
+
+void NMI_Handler(void){
+
+}
+
+void HardFault_Handler(void){
+
+    while(1)
+    {
+    }
+}
+void MemManage_Handler(void){
+
+}
+void BusFault_Handler(void){
+
+}
+void UsageFault_Handler(void){
+
+}
+void SVC_Handler(void){
+
+}
+void DebugMon_Handler(void){
+
+}
+void PendSV_Handler(void){
+
+}
+void SysTick_Handler(void){
+
+}
